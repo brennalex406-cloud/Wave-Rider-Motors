bc99e9e19e9a0ab6cca94069f3952f767fe2c5b5
brennalex406-cloud <brennalex406@gmail.com>
2026-03-13 17:53:22 -0700

    Movement

---
+++ Assets/scripts/boat.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boat : MonoBehaviour
+{
+    /*public float speed = 10f;
+    public float turnSpeed = 50f;
+
+    
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * move * speed * Time.deltaTime);
+        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
+    }*/
+    private Rigidbody rb;
+    public float speed = 20f;
+    public float turnSpeed = 50f;
+    public float acceleration = 20f;
+    public float maxSpeed = 20f;
+
+    public float currentSpeed = 0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        rb.freezeRotation = true;
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if(vertical != 0)
+        {
+            currentSpeed = Mathf.Clamp(currentSpeed + vertical * acceleration * Time.fixedDeltaTime, -maxSpeed, maxSpeed);
+
+        }
+        else
+        {
+            currentSpeed = Mathf.Lerp(currentSpeed, 0, 0.1f);
+        }
+
+        Vector3 moveDirection = -transform.right * currentSpeed;
+        rb.velocity = new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z);
+
+        if (currentSpeed != 0)
+        {
+            float turn = horizontal * turnSpeed * Time.fixedDeltaTime;
+            Quaternion turnRotation = Quaternion.Euler(0, turn, 0);
+            rb.MoveRotation(rb.rotation * turnRotation);
+        }
+    }
+}
